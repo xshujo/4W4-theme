f12a64e86336f02ebc36590aa274b271630b3492
Taïsha Dorval-Mbele <xshujo@gmail.com>
2023-02-03 09:29:55 -0500

    S2C2 - Début du footer (_pied_page.scss)

--- footer.php
+++ footer.php
@@ -1,5 +1,11 @@
-    <footer>
-        <h3>Le pied de page</h3>
+    <footer class="site__footer">
+        <!-- Balise*Nbr + Enter (ex: section*3 + Enter) crée la balise demandée
+             le nombre de fois demandé. Les balises de type inline sont placées
+             sur la même ligne tandis que les balises de type block sont placées
+             seules sur leur propre ligne -->
+        <section>Colonne 1</section>
+        <section>Colonne 2</section>
+        <section>Colonne 3</section>
     </footer>
     <?php wp_footer(); ?>
 </body>
