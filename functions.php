de38180897eb13ce08d7af17266e7da6a683089c
Taïsha Dorval-Mbele <xshujo@gmail.com>
2023-02-06 13:53:46 -0500

    S3C1 - add_theme_support(), ajout d'un logo, modification du style

--- functions.php
+++ functions.php
@@ -24,3 +24,9 @@ function enregistrement_nav_menu()
 }
 add_action('after_setup_theme', 'enregistrement_nav_menu', 0);
 // }
+
+/* add_theme_support() */
+add_theme_support('title-tag');
+add_theme_support('custom-logo', array('height' => 150,
+                                       'width' => 150));
+add_theme_support('post-thumbnails');
