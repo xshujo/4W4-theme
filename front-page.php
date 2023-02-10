3a3223b89daf766c4ce1450bd6c93d46c3e049c9
Taïsha Dorval-Mbele <xshujo@gmail.com>
2023-02-10 10:05:42 -0500

    Modification des styles (ajout des composants main et html pour modifier le corps du site)

--- front-page.php
+++ front-page.php
@@ -5,18 +5,21 @@
 ?>
 <?php get_header(); ?>
 <main>
-    <h3>front-page.php</h3>
+    <!-- <h3>front-page.php</h3> -->
     <?php
         // ":" + "endif" remplacent "{}"
         if (have_posts()):
             while (have_posts()) : the_post();?>
-            <h1>
-                <a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
-            </h1>
-            <?php // the_content(); // Affiche le contenu de l'article au comlet ?>
-            <?php // the_excerpt() // Affiche un résumé de l'article ?>
-            <?= wp_trim_words(get_the_excerpt(), 10, "&#10148;") ?>
-            <hr>
+            <section>
+                <h1>
+                    <a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
+                </h1>
+                <?php // the_content(); // Affiche le contenu de l'article au comlet ?>
+                <?php // the_excerpt() // Affiche un résumé de l'article ?>
+                <?= wp_trim_words(get_the_excerpt(), 10, " &#10148;") ?>
+                <hr>
+            </section>
+            
             <?php
             endwhile;
         endif;
