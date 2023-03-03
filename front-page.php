bf5fb0c7aa194ce30539e1a88ee1012474c75ae1
Taïsha Dorval-Mbele <xshujo@gmail.com>
2023-03-03 11:04:11 -0500

    S6C2 - Changement du display du main de grid à flex

--- front-page.php
+++ front-page.php
@@ -12,11 +12,11 @@
         // ":" + "endif" remplacent "{}"
         if (have_posts()) :
             while (have_posts()) : the_post();
+                $la_categorie = '4w4';
                 if (in_category('galerie')) {
-                    get_template_part('template-parts/categorie', 'galerie');
-                } else {
-                    get_template_part('template-parts/categorie', '4w4');
+                    $la_categorie = 'galerie';
                 }
+                get_template_part('template-parts/categorie', $la_categorie);
         ?>
         <?php
             endwhile;
