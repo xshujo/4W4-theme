6168fd1060573c6afc7aa8c2f2ae012f54e1f068
Taïsha Dorval-Mbele <xshujo@gmail.com>
2023-03-10 10:49:52 -0500

    S7C2 - Ajout d'un bouton burger

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,17 +8,30 @@
     <meta name="description" content="">
     <?php wp_head(); ?>
 </head>
-<body class="site <?= (is_front_page() ? 'no-aside' : '') ?>">
+<?php
+    $nouvelle_classe = "";
+    if (is_front_page()) {
+        $nouvelle_classe = 'no-aside';
+    }
+?>
+
+<body class="site <?= $nouvelle_classe ?>">
     <header class="site__header">
         <section class="site__header__logo">
             <!-- <div class="logomenu"> -->
-                <?php the_custom_logo() ?>
-                <?php wp_nav_menu(array(
-                    "menu" => "entete",
-                    "container" => "nav"
-                )) ?>
+            <?php the_custom_logo() ?>
+
+            <input type="checkbox" id="checkMenu">
+            <?php wp_nav_menu(array(
+                "menu" => "entete",
+                "container" => "nav"
+            )) ?>
             <!-- </div> -->
+
             <?php get_search_form(); ?>
+            <label class="burger" for="checkMenu">
+                <img src="https://s2.svgbox.net/hero-solid.svg?ic=menu&color=000" width="32" height="32">
+            </label>
         </section>
         <h1><a href="<?= bloginfo('url') ?>"><?= bloginfo('name') ?></a></h1>
         <h2><?= bloginfo('description') ?></h2>
